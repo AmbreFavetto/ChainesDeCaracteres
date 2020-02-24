9ca9a7274e02163d5d9d1743b5cbbd6d96c1a9a5
AmbreFavetto <47179688+AmbreFavetto@users.noreply.github.com>
2020-02-24 16:37:49 +0100

    Tests #4 "TU"
    Copies - tests unitaires.

---
+++ PARTIE1 (TU)/Copie/02.stpcpy/04.srcSMALL/srcSMALL_stpcpy.c
@@ -0,0 +1,49 @@
+/*
+ * Test de la valeur de retour et de la chaîne destination
+ * en situation normale
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#define BUFSIZE 10
+
+/* chercher le caractère nul dans la chaîne et pas au delà du buffer */
+void verif(char * s, int bufsize) {
+    char * p;
+    int i = 0;
+    int ok = 0;
+    for (p = s; i < bufsize; p++) {
+	if (*p == '\0' && i == strlen(s)) {
+           printf("'\\0' present -- position de '\\0' : %d\n",i); 
+	   ok = 1;
+        } 
+	i++;
+    }
+    if (ok == 0) {
+	printf("'\\0' non present\n"); 
+    }
+}
+
+int main (void) {
+
+    const char * src = "1234a";
+
+    char dst[BUFSIZE];
+
+    char * ret;
+
+    ret = stpcpy(dst,src);
+    ret--;		  
+
+    printf("stpcpy -- cas : NORMAL -- \n");
+    printf("ret (ATTENDU) : a -- ret (REEL) : ");
+    printf("%c\n",*ret);
+
+    printf("dst (ATTENDU) : 1234a -- dst (REEL) : ");
+    printf("%s\n",dst);
+
+    verif(dst,BUFSIZE);
+
+    return 0;
+}
